a5964cd55c21a6ea09dea6bc493a00ce02dfd479
reconelement <omkarpanda895@gmail.com>
2026-02-06 03:29:56 +0530

    Added async runtime support
    Added async runtime support

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,34 @@
+use std::io::Read;
+use std::ops::{Deref, DerefMut};
+use std::pin::Pin;
 use websocket::futures::Future;
 use websocket::{ClientBuilder};
 use websocket::client::Url;
 use websocket::r#async::client::{Client, ClientNew};
-fn main() {
+// fn main() {
+//     let ws_url = &Url::parse("wss://ws.backpack.exchange").unwrap();
+//     let mut connection = ClientBuilder::from_url(ws_url);
+//     let client = connection.connect_insecure().unwrap().into_stream();
+//     let _values = client.0.bytes().map(|item|{
+//         match item {
+//             Ok(item) => println!("{:#?}",item.to_string()),
+//             Err(_error) => panic!("Error")
+//         }
+//     });
+//     let mut async_client = connection.async_connect_insecure();
+
+// }
+
+#[tokio::main]
+async fn main(){
     let ws_url = &Url::parse("wss://ws.backpack.exchange").unwrap();
     let mut connection = ClientBuilder::from_url(ws_url);
-    let client = connection.connect_insecure().unwrap().into_stream();
-    let _values = client.0.bytes().map(|item|{
-        match item {
-            Ok(item) => println!("{:#?}",item.to_string()),
-            Err(_error) => panic!("Error")
-        }
-    });
+    //asynchronous connection 
     let mut async_client = connection.async_connect_insecure();
-
-}
-fn unbox<T>(value: Box<T>) -> T {
-    
+    let mut stream = async_client.deref_mut().wait();
+    let mut stream = stream.unwrap().0.into_inner();
+    //read 
+    let mut buf = [0;10];
+    let value = stream.read(&mut buf).unwrap();
+    println!("{:#?}",value);
 }
